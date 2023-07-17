b62fc886d60c8896e643b177d690aa416ae0985f
Nikolay <win32jeefoo@rambler.ru>
2023-07-17 13:01:30 +0300

    Добавляет функцию Делу - Время

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 function checkLength(string, requiredLength) {
   return string.length <= requiredLength;
 }
@@ -20,3 +21,26 @@ function isPalindrom(string) {
 }
 
 isPalindrom('какая-то строка');
+
+isNumber('функция 2023');
+
+function isOutOfTimeLimit(workStart, workEnd, meetingStart, meetingTime) {
+
+  const timeStrings = [workStart, workEnd, meetingStart];
+
+  const totalMinutes = timeStrings.map((timeString) => (
+    (([hours, minutes]) => hours * 60 + minutes)(timeString.split(':').map(Number))
+  ));
+
+  totalMinutes.push(parseInt(meetingTime, 10));
+
+  if (totalMinutes[2] + meetingTime > totalMinutes[1]) {
+    return false;
+  } else if (totalMinutes[2] < totalMinutes[0]) {
+    return false;
+  }
+
+  return true;
+}
+
+isOutOfTimeLimit('08:00', '17:30', '14:00', 90);
